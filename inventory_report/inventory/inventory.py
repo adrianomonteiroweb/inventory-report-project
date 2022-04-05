2b9e0952955dde5ca2ab9a6734ea796a9f9ef981
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-04-04 21:10:05 -0300

    feat: requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -13,6 +14,11 @@ class Inventory:
                 reader_csv = csv.DictReader(file, delimiter=",", quotechar='"')
                 list_csv = [row_csv for row_csv in reader_csv]
                 return Inventory.conditional_type(list_csv, report_type)
+
+        elif file_type[-1] == "json":
+            with open(file, 'r') as file:
+                list_json = json.load(file)
+                return Inventory.conditional_type(list_json, report_type)
 
     def conditional_type(file, report_type):
         if report_type == "simples":
